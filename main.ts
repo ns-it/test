a3938c72d8838efe16e82893ed6de9e5541a39df
Nawzat <nawzat.sibai@gmail.com>
2026-03-27 23:32:26 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+led.plot(1, 2)
+music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
+basic.showIcon(IconNames.Heart)
+basic.showNumber(5)
+basic.forever(function () {
+	
+})
